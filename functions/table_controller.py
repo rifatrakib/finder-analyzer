d446fed9865fc0e7fb416f1a2c70bec87739d4ea
rifatrakib <Rakib.1508@outlook.com>
2022-08-17 05:57:34 +0600

    build query incrementally based on queries provided

--- functions/table_controller.py
+++ functions/table_controller.py
@@ -18,20 +18,23 @@ def fetch_table_data(uri, payload):
     query_parameters = payload.get("query_parameters", {})
     sort_parameters = payload.get("sort_parameters", {})
     page_number = payload.get("page_number", 1)
+    pipeline = []
     
-    match_query = build_match_query(query_parameters)
-    sort_query = table_sort_query(sort_parameters)
+    if query_parameters:
+        match_query = build_match_query(query_parameters)
+        pipeline.append({"$match": match_query})
+    if sort_parameters:
+        sort_query = table_sort_query(sort_parameters)
+        pipeline.append({"$sort": sort_query})
     offset = (page_number - 1) * ITEM_PER_PAGE
     
-    pipeline = [
-        {"$match": match_query},
-        {"$sort": sort_query},
+    pipeline.extend([
         {"$skip": offset},
         {"$limit": ITEM_PER_PAGE},
         {"$project": {"_id": 0}}
-    ]
+    ])
     
     with MongoConnectionManager(uri, "Dashboard", "listing_collection") as collection:
-        data = dict(collection.aggregate(pipeline))
+        data = list(collection.aggregate(pipeline))
     
     return data
