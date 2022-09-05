6ec685c47584d2c7dd52aebdcf7e0b7cf90f7ab9
rifatrakib <Rakib.1508@outlook.com>
2022-09-05 13:26:34 +0600

    integrate geospatial query with mongodb

--- functions/table_controller.py
+++ functions/table_controller.py
@@ -1,15 +1,22 @@
-from functions.utils import MongoConnectionManager, build_match_query
+from functions.utils import MongoConnectionManager, build_match_query, build_geospatial_query
 
 ITEM_PER_PAGE = 50
 
 
-def aggregation_sort_query(sort_parameters):
+def build_table_sort_query(sort_parameters, geoquery=False):
     sort_fields = sort_parameters["sort_fields"][::-1]
     sort_directions = sort_parameters["sort_directions"][::-1]
     
-    sort_query = {}
+    if geoquery:
+        sort_query = list()
+    else:
+        sort_query = dict()
+    
     for field, direction in zip(sort_fields, sort_directions):
-        sort_query[field.upper()] = int(direction)
+        if geoquery:
+            sort_query.append((field.upper(), int(direction)))
+        else:
+            sort_query[field.upper()] = int(direction)
     
     return sort_query
 
@@ -22,7 +29,7 @@ def use_aggregation_pipeline(uri, query_parameters, sort_parameters, page_number
     pipeline.append({"$match": match_query})
     
     if sort_parameters:
-        sort_query = aggregation_sort_query(sort_parameters)
+        sort_query = build_table_sort_query(sort_parameters)
         pipeline.append({"$sort": sort_query})
     
     pipeline.extend([
@@ -37,6 +44,23 @@ def use_aggregation_pipeline(uri, query_parameters, sort_parameters, page_number
     return data
 
 
+def use_geospatial_pipeline(uri, query_parameters, sort_parameters, bbox, page_number):
+    offset = (page_number - 1) * ITEM_PER_PAGE
+    sort_query = None
+    query = build_geospatial_query(bbox, query_parameters)
+    
+    if sort_parameters:
+        sort_query = build_table_sort_query(sort_parameters, geo_query=True)
+    
+    with MongoConnectionManager(uri, "realestate", "listings") as collection:
+        if sort_query:
+            data = list(collection.find(query, {'_id': 0}).sort(sort_query).skip(offset).limit(50))
+        else:
+            data = list(collection.find(query, {'_id': 0}).skip(offset).limit(50))
+    
+    return data
+
+
 def fetch_table_data(uri, payload):
     query_parameters = payload.get("query_parameters", {})
     sort_parameters = payload.get("sort_parameters", {})
@@ -44,7 +68,7 @@ def fetch_table_data(uri, payload):
     bbox = payload.get("bbox", None)
     
     if bbox:
-        pass
+        data = use_geospatial_pipeline(uri, query_parameters, sort_parameters, bbox, page_number)
     else:
         data = use_aggregation_pipeline(uri, query_parameters, sort_parameters, page_number)
     
